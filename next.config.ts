0a987e8d3c083a9b869b9809709f31273717c978
Matty Serwer <Matthew.Serwer@gmail.com>
2025-06-11 04:34:07 -0400

    updates next.config.ts to optimize for a standalone next.js web service on render.com." " ;

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,43 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
-/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  devIndicators: {
+  output: 'standalone',
+  env: {
+    PORT: process.env.PORT || '10000',
+  },
+  poweredByHeader: false,
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          }
+        ]
+      }
+    ]
   }
 };
 
